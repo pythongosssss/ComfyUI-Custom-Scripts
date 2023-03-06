9ad6dbd90eecda0b8f680352bff04af6f5b6c28c
pythongosssss <125205205+pythongosssss@users.noreply.github.com>
2023-03-06 19:25:36 +0000

    Create graphArrange.js

---
+++ auto-arrange-graph/graphArrange.js
@@ -0,0 +1,15 @@
+import { app } from "../../scripts/app.js";
+
+// Adds right click -> arrange to the canvas
+
+app.registerExtension({
+	name: "pysssss.GraphArrange",
+	setup(app) {
+		const orig = LGraphCanvas.prototype.getCanvasMenuOptions;
+		LGraphCanvas.prototype.getCanvasMenuOptions = function () {
+			const options = orig.apply(this, arguments);
+			options.push({ content: "Arrange", callback: () => graph.arrange() });
+			return options;
+		};
+	},
+});
